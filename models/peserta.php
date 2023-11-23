90c459e02138a6acbf6c45da3770c9a503fc6f3f
Giant RJ <giant.r.j@gmail.com>
2023-11-23 10:40:00 +0700

    add controller and model peserta

--- models/peserta.php
+++ models/peserta.php
@@ -1,31 +1,37 @@
-
 <?php
-class Peserta {
-    public function getAllPeserta() {
-        return R::findAll('peserta');
+class Peserta
+{
+    public function getAllPeserta()
+    {
+        return R::findAll('peserta', 'isdelete = ?', [false]);
     }
 
-    public function getPesertaById($id) {
-        return R::load('peserta', $id);
+    public function getPesertaById($id)
+    {
+        return R::findOne('peserta', 'id = ? AND isdelete = ?', [$id, false]);
     }
 
-    public function createPeserta($nama, $usia) {
+    public function createPeserta($nama, $usia)
+    {
         $peserta = R::dispense('peserta');
         $peserta->nama = $nama;
         $peserta->usia = $usia;
+        $peserta->isdelete = false;
         R::store($peserta);
     }
 
-    public function updatePeserta($id, $nama, $usia) {
+    public function updatePeserta($id, $nama, $usia)
+    {
         $peserta = R::load('peserta', $id);
         $peserta->nama = $nama;
         $peserta->usia = $usia;
         R::store($peserta);
     }
 
-    public function deletePeserta($id) {
+    public function deletePeserta($id)
+    {
         $peserta = R::load('peserta', $id);
-        R::trash($peserta);
+        $peserta->isdelete = true;
+        R::store($peserta);
     }
 }
-?>
