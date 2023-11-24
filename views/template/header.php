ff1e2729b4172e7b00a65df0c033a55df71fd6a2
Giant RJ <giant.r.j@gmail.com>
2023-11-24 07:09:15 +0700

    add header and footer template

---
+++ views/template/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Delete Peserta</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css">
+</head>
+<body>
