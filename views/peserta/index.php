dacdd0c82efc8b4c1ed691aff50afa86c479673c
hakimahwadhha <argafanininda@gmail.com>
2023-12-03 11:03:01 +0700

    add button filter and update table

--- views/peserta/index.php
+++ views/peserta/index.php
@@ -1,31 +1,36 @@
-    <div class="container mt-5">
-        <h2>Peserta List</h2>
+      <div class="container mt-5">
+          <h2>Peserta List</h2>
+          <!-- Display filter button-->
+          <div>
+              <button>Filter</button>
+          </div>
+          <div>
+              <!-- Display Peserta data using Bootstrap table -->
+              <table class="table">
+                  <thead>
+                      <tr>
+                          <th>Nama</th>
+                          <th>Usia</th>
+                          <th>Action</th>
+                      </tr>
+                  </thead>
+                  <tbody>
+                      <?php foreach ($peserta as $row): ?>
+                      <tr>
+                          <td><?php echo $row['nama']; ?></td>
+                          <td><?php echo $row['usia']; ?></td>
+                          <td>
+                              <a href="<?php echo(SERVER_HOST); ?>/peserta/update/<?php echo $row['id']; ?>"
+                                  class="btn btn-primary">Update</a>
+                              <a href="<?php echo(SERVER_HOST); ?>/peserta/delete/<?php echo $row['id']; ?>"
+                                  class="btn btn-danger">Delete</a>
+                          </td>
+                      </tr>
+                      <?php endforeach; ?>
+                  </tbody>
+              </table>
 
-        <!-- Display Peserta data using Bootstrap table -->
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>Nama</th>
-                    <th>Usia</th>
-                    <th>Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach ($peserta as $row): ?>
-                    <tr>
-                        <td><?php echo $row['nama']; ?></td>
-                        <td><?php echo $row['usia']; ?></td>
-                        <td>
-                            <a href="<?php echo(SERVER_HOST); ?>/peserta/update/<?php echo $row['id']; ?>" class="btn btn-primary">Update</a>
-                            <a href="<?php echo(SERVER_HOST); ?>/peserta/delete/<?php echo $row['id']; ?>" class="btn btn-danger">Delete</a>
-                        </td>
-                    </tr>
-                <?php endforeach; ?>
-            </tbody>
-        </table>
-
-        <!-- Link to the create page -->
-        <a href="<?php echo(SERVER_HOST); ?>/peserta/create" class="btn btn-success">Create Peserta</a>
-   
-    </div>
-
+              <!-- Link to the create page -->
+              <a href="<?php echo(SERVER_HOST); ?>/peserta/create" class="btn btn-success">Create Peserta</a>
+          </div>
+      </div>
