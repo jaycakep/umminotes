2a585fab91eccba9348db0ee5c19bd61802d25e9
hakimahwadhha <argafanininda@gmail.com>
2023-12-04 01:34:03 +0700

    update UI Homepage and update css file

--- views/view_index.php
+++ views/view_index.php
@@ -1,16 +1,41 @@
-<div class="container mt-5">
-    <h2>ini view index yang sementara</h2>
-    <!-- Display Login User -->
-    <div>
-        <div>
-            <label for="nama" class="form-label">Nama</label>
-            <input type="text" class="form-control" id="nama" name="nama" required>
+<div class="container mt-5 px-5">
+    <div class="row justify-content-md-center gx-5 p-5 ">
+        <div class="col col-lg-2">
         </div>
-        <div><label for="password" class="form-label">Password</label>
-            <input type="text" class="form-control" id="password" name="password" required>
+        <div class="col-md-auto border bg-light justify-content-md-center gx-5 p-5">
+            <h3>Halo, selamat datang!</h3>
+            <p>Silahkan masukkan username dan password Anda.</p>
+            <!-- Display Login User -->
+            <form class="row g-2">
+                <div class="row-auto">
+                    <label for="username" class="form-label">Username</label>
+                    <input type="text" class="form-control" id="username" name="username" required>
+                </div>
+                <div class="row-auto">
+                    <label for="password" class="form-label">Password</label>
+                    <input type="text" class="form-control" id="password" name="password" required>
+                </div>
+                <div>
+                    <button type="submit" class="btn btn-primary">Login</button>
+                </div>
+            </form>
         </div>
-        <div>
-            <button type="submit" class="btn">Login</button>
+        <div class="col col-lg-2">
         </div>
     </div>
 </div>
+
+
+<!--<div class="container">
+    <div class="row justify-content-md-center">
+        <div class="col col-lg-2">
+            1 of 3
+        </div>
+        <div class="col-md-auto">
+            Variable width content
+        </div>
+        <div class="col col-lg-2">
+            3 of 3
+        </div>
+    </div>
+</div>v -->
