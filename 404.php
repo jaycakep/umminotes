05d22bacb6e6b8af1770bb6d984730721c653525
hakimahwadhha <argafanininda@gmail.com>
2023-12-06 06:57:27 +0700

    edit 404

--- 404.php
+++ 404.php
@@ -1,11 +1,28 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>404 Not Found</title>
+    <link rel="stylesheet" href="<?php echo (SERVER_HOST); ?>/assets/css/bootstrap.min.css">
 </head>
+
 <body>
-    <h1>404 Not Found</h1>
-    <p>Maaf, halaman yang Anda cari tidak dapat ditemukan.</p>
-    belum di edit
+    <div class="container bg-light mt-5 p-5 shadow-sm">
+        <!--<div class="row"></div>-->
+        <div class="row">
+            <div class="col-lg-2">
+            </div>
+            <div class="col p-5">
+                <h1 class="text-danger">404 Not Found</h1>
+                <p class="mt-5 text-dark">
+                    Maaf, UmmiNotes sedang dalam pengembangan. Halaman yang Anda cari tidak dapat ditemukan.
+                </p>
+            </div>
+        </div>
+        <div class="col-lg-2">
+        </div>
+    </div>
+    </div>
 </body>
+
 </html>
