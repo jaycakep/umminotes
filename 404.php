ff066f71be29d50e1658d84eed1ff48e086cef55
hakimahwadhha <argafanininda@gmail.com>
2023-12-11 05:19:18 +0700

    edit 404 fix

--- 404.php
+++ 404.php
@@ -7,13 +7,14 @@
 </head>
 
 <body>
-    <div class="container mt-5">
+    <div class="container mt-6">
         <div class="row bg-light mt-5 p-5 shadow-sm text-center">
             <div class="col p-5">
-                <h1 class="text-primary mb-5">404 Not Found</h1>
-                <img class="img-fluid" src="<?php echo (SERVER_HOST); ?>/assets/img/404.jfif" alt="Error 404" />
-                <p class="mt-5 text-dark">
-                    Maaf, UmmiNotes sedang dalam pengembangan. Halaman yang Anda cari tidak dapat ditemukan.
+                <h1 class="text-danger mb-5 h1-1">404</h1>
+                <!--<img class="img-fluid img-fluid-l" src="<?php echo (SERVER_HOST); ?>/assets/img/404.svg"
+                    alt="Error 404" />-->
+                <p class="mt-5 text-dark h6">
+                    Maaf, UmmiNotes sedang dalam pengembangan.
                 </p>
             </div>
         </div>
